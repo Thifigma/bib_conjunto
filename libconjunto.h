82d371d3920bf71b147b3e2fce7baf6a69714f28
thifigma <thiago.figueiredo@ufpr.br>
2022-12-05 21:20:20 -0300

    trabalhando uniao e conjuntos

--- libconjunto.h
+++ libconjunto.h
@@ -1,6 +1,8 @@
 #include <stdio.h>
 #include <stdlib.h>
 
+//Para facilitar considerei que todos os vetores têem o mesmo tamanho.
+
 void verifica_vazio (int *conjunto, int *n)
 {
     int i;
@@ -34,8 +36,27 @@ void cria_conjunto (int *conjunto, int *n)
         conjunto[i] = rand () %42;
 }
 
-void uniao (int *conj1, int *conj2, int *tam_conj1) 
+int tamanho (int conj1,int conj2, int tam)
 {
+    int novo_tam = 0;
     int i;
-    for (i = tam_conj1; i < (tam_conj1*2; )
+    for (i = 0; i < tam; i ++)
+    {
+        if (conj1[i] = conj2[i])
+            tam = tam + 1;
+    }
+
+    return novo_tam;    
 }
+
+void uniao (int *conj1, int *conj2, int *tam_conj1)  // Uniao so coloca o que não tem, entao tem que verificar se no cj1 tem os [] do cj2 se tiver nao poem. Logo como determinaro tamanho do vetor se nao sei direito quais tem e quais não tem?
+{
+    tamanho (conj1, conj2);  //verifica os conteudos para retornar o novo tamanho do conjunto.
+    int i;
+
+    for (i = tam_conj1; i < (tam_conj1 + tamanho(conj1, conj2); i__ ) )
+        conj1[tam_conj1] =                                               //Como verificar que conjunto ja estava e qual n estava?
+
+}
+
+void intersec () // so coloca os que estao nos dois.
